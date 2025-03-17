e0e082ced1462cd4c1e2469b405fb664802b89e0
Pankaj Garg <pagarg@adobe.com>
2025-03-17 16:09:55 +0530

    Scroll effect UI

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -175,8 +175,8 @@ function handleClassScrollFocus() {
         }
       } else {
         // Reset to default with animation when out of view
-        entry.target.style.transition = 'background-color 0.5s ease';
-        entry.target.style.backgroundColor = '#fffef9';
+        //entry.target.style.transition = 'background-color 0.5s ease';
+        //entry.target.style.backgroundColor = '';
       }
     });
   }, {
