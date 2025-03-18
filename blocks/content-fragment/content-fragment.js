6a583dec690fd521f058b4b4da16922c0cc0da99
praffuls <praffuls@adobe.com>
2025-03-18 18:00:56 +0530

    Content Fragment

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -17,19 +17,17 @@ export default async function decorate(block) {
     
     block.innerHTML = `
       <div class="banner-content" data-aue-type="reference" data-aue-filter="cf">
-        <div class="banner-container" style="background-image: url('${cfReq.bannerimage}')">
+        <div class="banner-container" style="background-image: url('${cfReq.bannerimage}'); min-height: 670px">
           <div class="banner-overlay">
             <div class="banner-text-content">
-              ${cfReq.preTitle ? `<p data-aue-prop="preTitle" data-aue-type="text" class="pretitle">${cfReq.preTitle}</p>` : ''}
               ${cfReq.title ? `<h1 data-aue-prop="title" data-aue-type="text" class="headline">${cfReq.title}</h1>` : ''}
               ${cfReq.subtitle ? `<h2 data-aue-prop="subtitle" data-aue-type="text" class="subtitle">${cfReq.subtitle}</h2>` : ''}
               ${cfReq.description ? `<p data-aue-prop="description" data-aue-type="text" class="description">${cfReq.description}</p>` : ''}
-              ${cfReq.shortDescription ? `<p data-aue-prop="shortDescription" data-aue-type="text" class="short-description">${cfReq.shortDescription}</p>` : ''}
-              ${cfReq.ctaRedirectLink ? `
+              ${cfReq.ctaurl ? `
                 <div class="button-container">
-                  <a data-aue-prop="ctaUrl" data-aue-type="text" href="${cfReq.ctaRedirectLink}" 
-                     title="${cfReq.ctaButtonText}" class="cta-button">
-                    ${cfReq.ctaButtonText}
+                  <a data-aue-prop="ctaUrl" data-aue-type="text" href="${cfReq.ctaurl}" 
+                     title="${cfReq.ctalabel}" class="cta-button">
+                    ${cfReq.ctalabel}
                   </a>
                 </div>
               ` : ''}
