7d18afaa020f5999b6e499e529202691cb164c7d
praffuls <praffuls@adobe.com>
2025-03-18 14:43:51 +0530

    Code update for Content Fragment Usgae in UE

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,24 +1,41 @@
 export default async function decorate(block) {
     console.log("Content Fragment component executed successfully");
-    const aempublishurl = 'https://publish-p140426-e1433687.adobeaemcloud.com/';
-    const aemauthorurl = 'https://author-p140426-e1433687.adobeaemcloud.com/';
     const offerpath = block.querySelector(':scope div:nth-child(1) > div a').innerHTML.trim();
-    const cfReq = await fetch("https://author-p148716-e1519766.adobeaemcloud.com"+offerpath+"/jcr%3Acontent/data/master.json")
+    const cfReq = await fetch("https://author-p140426-e1433687.adobeaemcloud.com/content/dam/mobile/en/fragments/default/jcr%3Acontent/data/master.json")
     .then((response) => response.json()).then((contentfragment) => {
       let offer = {};
       if (contentfragment) {
-        offer.ctaButtonText = contentfragment.ctaButtonText;
-        offer.ctaRedirectLink = contentfragment.ctaRedirectLink;
         offer.description = contentfragment.description;
-        offer.imagePath = contentfragment.imagePath;
-        offer.preTitle = contentfragment.preTitle;
-        offer.shortDescription = contentfragment.shortDescription;
+        offer.ctaurl = contentfragment.ctaurl;
+        offer.ctalabel = contentfragment.ctalabel;
+        offer.bannerimage = contentfragment.bannerimage;
         offer.title = contentfragment.title;
+        offer.subtitle = contentfragment.subtitle;
       }
       return offer;
     });
     
-    block.innerHTML = '<div class="banner-content" data-aue-type="reference" data-aue-filter="cf"><div data-aue-prop="heroImage" data-aue-type="media" class="banner-detail"><p data-aue-prop="title" data-aue-type="text" class="headline">'+ cfReq.title + '</p>'+
-        '<p data-aue-prop="description" data-aue-type="text" class="headline">'+cfReq.description +'</p><img src="'+cfReq.imagePath+'"></img><p data-aue-prop="shortDescription" data-aue-type="text" class="short-description">'+cfReq.shortDescription+'</p>'+
-        '<p data-aue-prop="preTitle" data-aue-type="text" class="pretitle">'+cfReq.preTitle+'</p><p class="button-container"><a data-aue-prop="ctaUrl" data-aue-type="text" href="'+cfReq.ctaRedirectLink+'" title="'+cfReq.ctaButtonText+'" class="hyperlink">'+cfReq.ctaButtonText+'</a></p></div></div>';
+    block.innerHTML = `
+      <div class="banner-content" data-aue-type="reference" data-aue-filter="cf">
+        <div class="banner-container" style="background-image: url('${cfReq.bannerimage}')">
+          <div class="banner-overlay">
+            <div class="banner-text-content">
+              ${cfReq.preTitle ? `<p data-aue-prop="preTitle" data-aue-type="text" class="pretitle">${cfReq.preTitle}</p>` : ''}
+              ${cfReq.title ? `<h1 data-aue-prop="title" data-aue-type="text" class="headline">${cfReq.title}</h1>` : ''}
+              ${cfReq.subtitle ? `<h2 data-aue-prop="subtitle" data-aue-type="text" class="subtitle">${cfReq.subtitle}</h2>` : ''}
+              ${cfReq.description ? `<p data-aue-prop="description" data-aue-type="text" class="description">${cfReq.description}</p>` : ''}
+              ${cfReq.shortDescription ? `<p data-aue-prop="shortDescription" data-aue-type="text" class="short-description">${cfReq.shortDescription}</p>` : ''}
+              ${cfReq.ctaRedirectLink ? `
+                <div class="button-container">
+                  <a data-aue-prop="ctaUrl" data-aue-type="text" href="${cfReq.ctaRedirectLink}" 
+                     title="${cfReq.ctaButtonText}" class="cta-button">
+                    ${cfReq.ctaButtonText}
+                  </a>
+                </div>
+              ` : ''}
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
   }
